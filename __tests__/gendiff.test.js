e4dc48ff9d3133ee970fa6642c4561cb12a1b02a
nadya <Kowalewskaya91@ya.ru>
2024-04-19 15:42:28 +0300

    Step 5 finished

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,8 +10,6 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-console.log(getFixturePath(`file1.json`));
-console.log(readFixture(`file1.json`));
 test('gendiff', () => {
-    expect(genDiff(parse(getFixturePath(`file1.json`)), parse(getFixturePath(`file1.json`)))).toEqual(readFixture(`expected_file_json.txt`));
+  expect(genDiff(parse(getFixturePath('file1.json')), parse(getFixturePath('file2.json')))).toEqual(readFixture('expected_file_json.txt'));
 });
--- src/comparison.js
+++ src/comparison.js
@@ -4,20 +4,18 @@ const makeComparison = (obj1, obj2) => {
   const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
   let result = '{\n';
   keys.forEach((key) => {
-    const val1 = obj1[key];
-    const val2 = obj2[key];
     if (key in obj2 && !(key in obj1)) {
-      result += ` - ${key}: ${val1}\n`;
+      result += `  + ${key}: ${obj2[key]}\n`;
     }
-    if (key in obj1 && key in obj2 && val1 !== val2) {
-      result += ` - ${key}: ${val1}\n`;
-      result += ` + ${key}: ${val2}\n`;
+    if (key in obj1 && key in obj2 && obj1[key] !== obj2[key]) {
+      result += `  - ${key}: ${obj1[key]}\n`;
+      result += `  + ${key}: ${obj2[key]}\n`;
     }
-    if (key in obj1 && key in obj2 && val1 === val2) {
-      result += `    ${key}: ${val1}\n`;
+    if (key in obj1 && key in obj2 && obj1[key] === obj2[key]) {
+      result += `    ${key}: ${obj1[key]}\n`;
     }
     if (key in obj1 && !(key in obj2)) {
-      result += ` + ${key}: ${val2}\n`;
+      result += `  - ${key}: ${obj1[key]}\n`;
     }
   });
   return `${result}}`;
